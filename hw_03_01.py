7962049cd7836126be96c936f7d3ca0e0daa4c6b
Dmytro Demydenko <realmalque@gmail.com>
2024-03-09 16:49:03 +0200

    2nd var

--- hw_03_01.py
+++ hw_03_01.py
@@ -1,8 +1,17 @@
-import datetime
+# import datetime
 
-def get_days_from_today(date):
-    today = datetime.datetime.today()
-    hw_deadline = datetime.datetime(year=2024, month=4, day=21, hour=23, minute=45)
-    difference = today - hw_deadline
-    print (difference.days)
-  
+# def get_days_from_today(date):
+#     today = datetime.datetime.today()
+#     hw_deadline = datetime.datetime(year=2024, month=4, day=21, hour=23, minute=45)
+#     difference = today - hw_deadline
+#     print (difference.days)
+
+
+# from datetime import datetime
+# # Встановлення дати 
+# hw_deadline = datetime(year=2024, month=4, day=21)
+# # Поточна дата
+# current_date = datetime.now()
+# # Розрахунок кількості днів
+# days_since = hw_deadline.toordinal() - current_date.toordinal()
+# print(days_since)
