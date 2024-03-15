bdde85ea535112ce9ff6437a75684e2994558982
Dmytro Demydenko <realmalque@gmail.com>
2024-03-15 14:17:11 +0200

    HW ready?

--- hw_03_01.py
+++ hw_03_01.py
@@ -1,6 +1,12 @@
+from datetime import datetime
 
-import datetime
-tday = datetime.date.today()
-deadline = datetime.date(2024, 4, 21)
-till_dl = deadline - tday
-print(till_dl.days)
+def get_days_from_today(date):
+    try:
+        hw_deadline = datetime.strptime(date, '%Y-%m-%d')
+        today = datetime.today()
+        delta = today - hw_deadline
+        return delta.days
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'."
+
+print(get_days_from_today("2024-04-21"))
