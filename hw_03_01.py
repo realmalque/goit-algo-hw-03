0472dee9184f57a74465923da8aea31cfb95aa6e
Dmytro Demydenko <realmalque@gmail.com>
2024-03-15 14:07:12 +0200

    HW ready?

--- hw_03_01.py
+++ hw_03_01.py
@@ -1,17 +1,6 @@
-# import datetime
 
-# def get_days_from_today(date):
-#     today = datetime.datetime.today()
-#     hw_deadline = datetime.datetime(year=2024, month=4, day=21, hour=23, minute=45)
-#     difference = today - hw_deadline
-#     print (difference.days)
-
-
-# from datetime import datetime
-# # Встановлення дати 
-# hw_deadline = datetime(year=2024, month=4, day=21)
-# # Поточна дата
-# current_date = datetime.now()
-# # Розрахунок кількості днів
-# days_since = hw_deadline.toordinal() - current_date.toordinal()
-# print(days_since)
+import datetime
+tday = datetime.date.today()
+deadline = datetime.date(2024, 4, 21)
+till_dl = deadline - tday
+print(till_dl.days)
