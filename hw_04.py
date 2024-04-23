ece6f6524d001acec6cbe314251c0f1870e1def8
Dmytro Demydenko <realmalque@gmail.com>
2024-04-23 13:52:33 +0300

    Задача 2_04 готова?!

--- hw_04.py
+++ hw_04.py
@@ -19,3 +19,22 @@ def total_salary(path):
 
 total, average = total_salary('salary_list.txt')
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+#_________________________________Задача_02_04________________________________________
+
+from pathlib import Path
+
+def get_cats_info(path):
+    with open('Cats_info.txt', 'r') as file:
+        try:
+            cats_info = list()
+            for line in file:
+                every_cat = {'id':line.split(',')[0],'name':line.split(',')[1],'age':line.split(',')[2].strip()}    
+                cats_info.append(every_cat)
+            return cats_info
+         
+        except OSError:
+            print('Error while reading file')
+
+cats_info = get_cats_info('Cats_info.txt')
+print(cats_info)
